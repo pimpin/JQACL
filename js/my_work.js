45d371d1c1c1dee9de87deee92b99f11df613da7
Pierre Merlin <pierrem@apido.org>
2012-05-04 14:32:50 +0200

    1.12 Delegate

--- js/my_work.js
+++ js/my_work.js
@@ -1,3 +1,6 @@
 $('div.seating-chart .economy-class li a.available').click(selectSeat);
 $('div.seating-chart .first-class li a.available').click(selectFirstClass);
-$('a.confirm-upgrade').live('click', confirmUpgrade);
+
+// $('a.confirm-upgrade').live('click', confirmUpgrade);
+// better with delegate (it permit method chaining and is more performant)
+$('#confirm-first-class').delegate('a.confirm-upgrade', 'click', confirmUpgrade);
